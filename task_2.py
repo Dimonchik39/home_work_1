e22d44cb9f5ce00ada5cc32c52562a1afcc3df33
Dimonchik <sudjadred@live.com>
2022-11-24 01:13:10 +0200

    add_task_2_update

--- task_2.py
+++ task_2.py
@@ -1,18 +1,20 @@
 # 2- Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 # Нужно написать таблицу истинности.
 
-X = int(input("Введите значение X: "))
-Y = int(input("Введите значение Y: "))
-Z = int(input("Введите значение Z: "))
-num = [X, Y, Z]
-
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[2] and not x[2] and not x[2]
-    result = left == right
-    return result
-
-if checkPredicate(num) == True:
-    print("Утверждение истинно")
+x = None
+y = None
+z = None
+a_side = not (x or y or z)
+b_side = not x and not y and not z
+if a_side == b_side:
+    print ('Истина')
 else:
-    print("Утверждение ложно")
+    print ('Ложь')
+
+print('x y z')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            result = a_side == b_side
+            if result == True:
+                print(x, y, z, result)
